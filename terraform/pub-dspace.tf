75c44ec8b56cf427f9636180840c09c86afca8e4
Hardy Pottinger <hardy.pottinger@gmail.com>
2021-02-22 12:03:55 -0600

    WIP updating code to most recent working version
    - Adds new role 'detail_oriented' which handles small changes
    - Improves reliability of the Solr provisioning
    - Adds required security group for the pub-ds-api Terraform config

--- terraform/pub-dspace.tf
+++ terraform/pub-dspace.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "pub-ds-api-dev" {
   ami      = "ami-0e472933a1395e172"
   instance_type = "t3.medium"
-  security_groups = ["SSH from bastion and UCOP VPN"]
+  security_groups = ["SSH from bastion and UCOP VPN", "pub-ds-dev"]
   key_name = "hpottinger-keypair"
   tags = {
     Name = "pub-ds-api"
@@ -26,3 +26,13 @@ resource "aws_instance" "pub-ds-client-dev" {
     Project = "dspace"
   }
 }
+
+resource "aws_eip" "pub-ds-api-dev" {
+  instance = aws_instance.pub-ds-api-dev.id
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id = aws_instance.pub-ds-api-dev.id
+  allocation_id = aws_eip.pub-ds-api-dev.id
+}
+
